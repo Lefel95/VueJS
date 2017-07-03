bb9b656f0d8c277e86e7fd432f53ce9fcf71b7fa
Felipe <lefel95@outlook.com>
2017-07-02 22:57:50 -0300

    Feito 4 Aulas

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ new Vue ({
     websiteTag: '<a href="https://felipe.com.br"> Outro esquema de Site </a>',
     idade: 21,
     x: 0,
-    y: 0
+    y: 0,
+    name: '',
+    age: '',
+    a: 0,
+    b: 0,
+    gender: ''
   },
 
   methods: {
@@ -38,7 +43,25 @@ new Vue ({
 
     logAge: function() {
       console.log('Voce digitou a sua idade');
+    },
+
+    changeGender: function(gender) {
+      this.gender = gender;
     }
 
+  },
+
+  computed: {
+    agePlusA: function() {
+      return this.a + this.idade;
+    },
+
+    agePlusB: function () {
+      return this.b + this.idade;
+    },
+
+    compClass: function() {
+      return gender = this.gender;
+    }
   }
 });
